ea2da2805b1b4f767050a25da52e95d97a09f26a
Pankaj Gupta <contact.gpankaj@gmail.com>
2016-12-24 20:51:46 +0530

    removed public key files

--- startApp.sh
+++ startApp.sh
@@ -2,7 +2,6 @@
 export ENVIRONMENT=TEST
 sudo rm -rf /tmp/all_current_envs
 env > /tmp/all_current_envs
-
 sudo /usr/bin/pip install -r requirements.txt
 python manage.py runserver --host 0.0.0.0 &
 exit 0
