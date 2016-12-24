e27d5d09fbdbbd0145380a839fb41bf21dc6ba45
Pankaj Gupta <contact.gpankaj@gmail.com>
2016-12-24 20:31:35 +0530

    restructure

--- startApp.sh
+++ startApp.sh
@@ -4,5 +4,5 @@ sudo rm -rf /tmp/all_current_envs
 env > /tmp/all_current_envs
 
 sudo /usr/bin/pip install -r requirements.txt
-python manage.py runserver &
+python manage.py runserver -h 0.0.0.0 &
 exit 0
