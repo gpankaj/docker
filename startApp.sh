b055cf34f3c23419649eeceadd64a04c5e28a775
Pankaj Gupta <contact.gpankaj@gmail.com>
2016-12-24 17:47:03 +0530

    Modifying yaml

--- startApp.sh
+++ startApp.sh
@@ -2,8 +2,3 @@
 export ENVIRONMENT=PRODUCTION
 
 env > /tmp/all_current_envs
-
-if [ "$DEPLOYMENT_GROUP_NAME" == "Staging" ]
-then
-    sed -i -e 's/Listen 80/Listen 9090/g' /etc/httpd/conf/httpd.conf
-fi
