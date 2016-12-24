3497822cb3afc5b2a0f699f8aab079551adbcbdd
Pankaj Gupta <contact.gpankaj@gmail.com>
2016-12-24 19:57:45 +0530

    adding sudo in script

--- startApp.sh
+++ startApp.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 export ENVIRONMENT=TEST
-rm -rf /tmp/all_current_envs
+sudo rm -rf /tmp/all_current_envs
 env > /tmp/all_current_envs
-sudo pip install -r requirements.txt
+
+sudo /usr/bin/pip install -r requirements.txt
 python manage.py runserver &
 exit 0
