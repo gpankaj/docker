b40e01800450b49efaed5aa305ad1629b5c3fee2
Pankaj Gupta <contact.gpankaj@gmail.com>
2016-12-25 14:06:09 +0530

    fixed str typo

--- app/blueprints/home/routes.py
+++ app/blueprints/home/routes.py
@@ -7,12 +7,13 @@ def index():
     all_envs = os.environ
     return render_template('home/index.html', all_envs=all_envs)
 
-@home.route('/debug')
-def index():
+
+@home.route('/debug/<string:file_name>', methods = ['GET', 'POST'])
+def debug(file_name):
     import os
     import configparser
     all_envs={}
-    with open('/tmp/all_vars') as f:
+    with open(file_name) as f:
         for line in f:
             values = line.split('=')
             all_envs[values[0]] = values[1]
